2511ed359ab0df19e89bbf894fc09748874bfcd7
Kalika <kalikavidyarthi111.kv@gmail.com>
2026-09-04 11:16:22 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    void display(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args){
+        Hello obj= new Hello();
+        obj.display();
+    }
+}
